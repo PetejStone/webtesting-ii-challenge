68f580e32a3a90569e1db09e31f2dbd2508168a3
PetejStone <petejstoney@gmail.com>
2019-06-18 12:37:40 -0700

    rendered without errorts test added with testing library imports

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Dashboard extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <h1>This is the dashboard</h1>
+        )
+    }
+}
---
+++ client/src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Display extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <h1>This is the display</h1>
+        )
+    }
+}
